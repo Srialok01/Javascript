a3abb968b1787234eab492a7ff19a2d65401c723
Alok Srivastav <30576388+Srialok01@users.noreply.github.com>
2023-08-28 17:01:44 +0000

    basics

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,18 @@
+const accountNo = 1234563
+let accountEmail = "alok.srivastav@fakeemail.com"
+var accountPassword = "ThisisP@ssword"
+address = "Ghazipur"
+let accountStatus;
+console.table([accountNo,accountEmail,accountPassword,address,accountStatus])
+
+console.log("Now changing values");
+
+// accountNo = 5432133  this is not allowed
+console.log(accountNo);
+accountEmail ="alok.srivastav@correctemail.com"
+accountPassword = "UpdatedP@ssw0rd"
+address = "Ghazipur"
+accountStatus = "active"
+
+
+console.table([accountNo,accountEmail,accountPassword,address,accountStatus])
