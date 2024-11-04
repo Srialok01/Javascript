3597979ca83067d314ce2c257ac1a58ce1e7639e
Alok Srivastav <30576388+Srialok01@users.noreply.github.com>
2024-11-04 19:07:17 +0000

    revisiting again

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -1,7 +1,13 @@
 const accountNo = 1234563
 let accountEmail = "alok.srivastav@fakeemail.com"
+/*
+Prefer not to use var, because of issue in block scope and functional scope
+*/
+
 var accountPassword = "ThisisP@ssword"
 address = "Ghazipur"
+console.log(typeof address)
+
 let accountStatus;
 console.table([accountNo,accountEmail,accountPassword,address,accountStatus])
 
