2e5c22226c883e02f7171bcabefdd15ceb346943
Alok Srivastav <30576388+Srialok01@users.noreply.github.com>
2024-11-04 19:32:40 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use-strict"; // treat all code as newer version of java script
+
+alert(2 + 2); // this will throw an error as we are using node and not browser
+
+//number => 12
+
+//bigInt
+
+//string
+
+//boolean
+
+//null => standalone value
+
+//undefined = >
+
+//symbol => unique
+
+// NON PRIMITIVE DATA TYPE
+
+//object
+
+console.log(typeof null) // object
+
+console.log(typeof undefined) // undefined
+
