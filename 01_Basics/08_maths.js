714830dfd35f9aca4032a5edc25a1c939c366a43
Alok Srivastav <30576388+Srialok01@users.noreply.github.com>
2024-11-09 18:30:41 +0000

    maths module

---
+++ 01_Basics/08_maths.js
@@ -0,0 +1,25 @@
+console.log("--------------------MATH--------------------")
+/*
+console.log(Math);
+console.log(Math.abs(-12)) // 12 -> converts negative to a positive value 
+
+console.log(Math.round(5.232)); //5
+
+console.log(Math.ceil(12.8437)) // 13
+
+console.log(Math.floor(12.8437)) // 12
+
+console.log(Math.min(2,5,10,1,53,4)); //1
+
+console.log(Math.max(2,5,10,1,53,4)); //53
+
+
+console.log(Math.random()) // random number between 0 and 1 
+console.log(Math.random()*10+1)  // random number between 1 and 10
+*/
+// Find value between 10 and 20
+
+const min = 100
+const max = 200
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
