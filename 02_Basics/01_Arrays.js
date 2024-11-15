c3b646327ec31c3f092b3defdc835c92e3889ce0
Alok Srivastav <30576388+Srialok01@users.noreply.github.com>
2024-11-15 18:21:57 +0000

    Array

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,9 @@
+
+const myArray = [0,1,3,5,true,'Alok']
+console.log(myArray[2]);
+
+// 2nd Method to declare an array
+const myHeros = new Array('John wick','Iron Man','Shaktimaan')
+console.log(myHeros[0]);
+console.log(typeof myHeros);
+
