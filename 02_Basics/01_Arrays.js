721a49e883c8ec4b2c32870be67ecede892df329
Alok Srivastav <30576388+Srialok01@users.noreply.github.com>
2024-11-16 10:26:13 +0000

    array

--- 02_Basics/01_Arrays.js
+++ 02_Basics/01_Arrays.js
@@ -1,5 +1,5 @@
 
-const myArray = [0,1,3,5,true,'Alok']
+const myArray = [0,1,3,'Alok']
 console.log(myArray[2]);
 
 // 2nd Method to declare an array
@@ -7,3 +7,14 @@ const myHeros = new Array('John wick','Iron Man','Shaktimaan')
 console.log(myHeros[0]);
 console.log(typeof myHeros);
 
+// Array methods
+console.log("---------PUSH------------");
+
+console.log(myArray);  // [0,1,3,'Alok']
+myArray.push(9)   
+console.log(myArray);  // [0,1,3,'Alok',9]
+
+console.log("---------POP------------");
+console.log(myArray);  // [0,1,3,'Alok',9]
+myArray.pop()
+console.log(myArray);  // [0,1,3,'Alok']
